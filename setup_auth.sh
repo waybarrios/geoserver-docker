20efb83429f275ed6b9acd9b3e09801201dcb7e1
Way Barrios <waybarrios@gmail.com>
2016-12-14 17:04:21 -0500

    Adding geonode authentication

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sed -i.bak 's@<baseUrl>\([^<][^<]*\)</baseUrl>@<baseUrl>'"$DJANGO_URL"'</baseUrl>@'\
-           /geoserver_data/data/security/auth/geonodeAuthProvider/config.xml
+
+sed -i 's/localhost:8000/'"$DJANGO_URL"'/g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
+sed -i 's/localhost:8080/'"$GEOSERVER_PUBLIC_LOCATION"'/g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
+sed -i 's/localhost:8000/'"$DJANGO_URL"'/g' /geoserver_data/data/security/role/geonode\ REST\ role\ service/config.xml
+
