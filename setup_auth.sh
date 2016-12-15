c5044e00ad5d0c1b460ba5e191f1788dcb57da07
Way Barrios <waybarrios@gmail.com>
2016-12-15 09:08:12 -0500

    Modified DJANGO_URL to BASE_URL and adding django public url

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's/localhost:8000/'"$DJANGO_URL"'/g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
+sed -i 's/localhost:8000/'"$DJANGO_PUBLIC_LOCATION"'/g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
 sed -i 's/localhost:8080/'"$GEOSERVER_PUBLIC_LOCATION"'/g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
-sed -i 's/localhost:8000/'"$DJANGO_URL"'/g' /geoserver_data/data/security/role/geonode\ REST\ role\ service/config.xml
+sed -i 's/localhost:8000/'"$BASE_URL"'/g' /geoserver_data/data/security/role/geonode\ REST\ role\ service/config.xml
 
